cdc0303461f22e4f9bc9280ebbd9d3255bef8bf1
wanderer120 <megamanx120@hotmail.com>
2020-11-26 14:56:29 +0800

    fix for super user comission

--- SingleNeko.sol
+++ SingleNeko.sol
@@ -2,6 +2,7 @@
 //["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"]
 // testnet MetaMask
 //contract v1 at testnet - 0xA817F715FaA3E7ee19FB41A2aA6585b06897cb51
+//contract v1.1 at testnet - 0xDb1fa87118627E1850B9144EEFd43B46b35F3eD9
 //0x73e612F58362f44Bb0Af24fA074B147b30389252   - owner at testnet
 //["0x3EbD46521802ab19A2411De9fe34C9cb7E6B3FA7","0xa010d14032A7e24f9374182E5663E743Dc66321F"]
 pragma solidity >=0.4.23 <0.6.0;
@@ -163,8 +164,15 @@ contract SingleNeko is IERC721, ERC165 {
         uint totalItem = users[referrerAddress].itemCount;
         if(totalItem % 4 == 0){
             //Last slot goes to super upline
-            giveETH(users[referrerAddress].referrer,uplineCommision);
-            emit SentExtraEthDividends(user, users[referrerAddress].referrer);
+            if(referrerAddress!=owner){
+              giveETH(users[referrerAddress].referrer,uplineCommision);
+              emit SentExtraEthDividends(user, users[referrerAddress].referrer);
+            }else{
+              giveETH(owner,uplineCommision);
+              emit SentExtraEthDividends(user, owner.referrer);
+            }
+
+
         }else{
             //goes to upline
             giveETH(referrerAddress,uplineCommision);
